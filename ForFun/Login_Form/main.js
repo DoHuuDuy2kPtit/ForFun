1730b11ecc34e53c0ffe4153dc798ddab3ef7905
Vũ Long <45234273+vulong2504@users.noreply.github.com>
2020-10-30 21:35:27 +0700

    Update main.js

--- ForFun/Login_Form/main.js
+++ ForFun/Login_Form/main.js
@@ -2,7 +2,7 @@ let emailRegex = /^[A-Za-z][\w\.]+@[a-z]{3,9}(\.[a-z]{3,6}){1,2}$/gm;
 
 let passRegex = /\w{6,32}/gm;
 
-function checkEmail(){
+function testConflict(){
     let emailInput = document.getElementById("email_address").value;
     let passInput = document.getElementById("email_password").value;
     if(!emailRegex.test(emailInput) || !passRegex.test(passInput)) 
